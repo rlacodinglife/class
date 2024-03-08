c55ca3c2e073559fa67c7977af0109d6bb8741f6
rlaguitar <rla960926@gmail.com>
2024-03-08 11:25:34 +0900

    reactclass3

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Product() {
-  return (
-    <div>Product</div>
-  )
+
+
+    const navigate=useNavigate()
+
+
+    const [idtext, setidtext] = useState('')
+
+    const changeidtext = (e) => {
+        setidtext(e.currentTarget.value)
+    }
+
+    const summitidtext = (e) => {
+        e.preventDefault()
+        navigate(`/product/${idtext}`)
+        setidtext('')
+    }
+    return (
+        <div>Product
+
+            <form onSubmit={summitidtext}>
+                <input type='text' placeholder='상품아이디 입력' style={{ width: 200, height: 25, margin: 20, textIndent: 10 }} onChange={changeidtext} value={idtext} />
+            </form>
+        </div>
+    )
 }
